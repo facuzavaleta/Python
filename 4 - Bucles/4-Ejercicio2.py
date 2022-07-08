398e85d924c5303dcdaf1da5a2ecf257646a134b
Facundo Zavaleta <33104730+facuzavaleta@users.noreply.github.com>
2022-07-08 17:04:41 -0300

    Delete 4 - Bucles directory

--- 4 - Bucles/4-Ejercicio2.py
+++
@@ -1,7 +0,0 @@
-edad = int(input("Ingrese su edad: "))
-for años in range(edad + 1):
-    if años == 1:
-        print("Usted cumplio", años, "año.")
-    if años > 1:
-        print("Usted cumplio", años, "años.")
-    
