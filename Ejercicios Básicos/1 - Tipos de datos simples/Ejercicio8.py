93dff19c702317de2e39817de118c9f4d3f63783
facuzavaleta <facuzavaleta890@gmail.com>
2023-08-24 19:19:00 -0500

    Ejercicio 1-8

---
+++ Ejercicios Básicos/1 - Tipos de datos simples/Ejercicio8.py
@@ -0,0 +1,6 @@
+n1 = int(input("Ingrese un numero entero: "))
+n2 = int(input("Ingrese otro numero entero: "))
+cociente = n1 // n2
+resto = n1 % n2
+
+print (f"El cociente de {n1} entre {n2} es de {cociente} y el resto es de {resto}.")
