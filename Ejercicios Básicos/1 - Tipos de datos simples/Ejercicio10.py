0098bc38b6d69e9f68f858c4f537d54d48c992ef
facuzavaleta <facuzavaleta890@gmail.com>
2023-08-27 11:15:30 -0500

    Ejercicio 1-10

---
+++ Ejercicios Básicos/1 - Tipos de datos simples/Ejercicio10.py
@@ -0,0 +1,9 @@
+peso_payaso = 112
+peso_muñeca = 75
+
+num_payasos = int(input("Cuantos payasos? " ))
+num_muñecas = int(input("Cuantas muñecas? "))
+
+peso_total = (peso_payaso * num_payasos) + (peso_muñeca * num_muñecas)
+
+print(f"El peso total del paquete es de {peso_total} kg.")
