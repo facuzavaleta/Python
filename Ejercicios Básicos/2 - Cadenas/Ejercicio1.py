06f694d7ad6104b50db2d91ea648ed63e9d57b33
facuzavaleta <facuzavaleta890@gmail.com>
2023-08-27 13:51:55 -0500

    Agregas instrucciones de ejercicios

--- Ejercicios Básicos/2 - Cadenas/Ejercicio1.py
+++ Ejercicios Básicos/2 - Cadenas/Ejercicio1.py
@@ -1,3 +1,7 @@
+"""Ejercicio 1
+Escribir un programa que pregunte el nombre del usuario en la consola y un número entero 
+e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido."""
+
 nombre = input("Ingrese su nombre: ")
 num = int(input("Ingrese la cantidad de veces que se va a imprimir: "))
 print((nombre + "\n") * num) 
--- Ejercicios Básicos/2 - Cadenas/Ejercicio5.py
+++ Ejercicios Básicos/2 - Cadenas/Ejercicio5.py
@@ -1,3 +1,7 @@
+"""Ejercicio 5
+Escribir un programa que pida al usuario que introduzca una frase en la consola 
+y muestre por pantalla la frase invertida."""
+
 frase = input("Ingrese una frase: ")
 
 print(frase[::-1])
