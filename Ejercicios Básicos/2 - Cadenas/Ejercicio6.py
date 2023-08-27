b6a7023077ea86fd9b24a3a15c1c8b3d891ea575
facuzavaleta <facuzavaleta890@gmail.com>
2023-08-27 13:58:54 -0500

    Ejercicio 2-6

---
+++ Ejercicios Básicos/2 - Cadenas/Ejercicio6.py
@@ -0,0 +1,7 @@
+"""Ejercicio 6
+Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal, 
+y después muestre por pantalla la misma frase pero con la vocal introducida en mayúscula."""
+
+frase = input("Ingrese una frase: ")
+vocal = input("Ingrese una vocal: ")
+print(frase.replace(vocal, vocal.upper()))
