fa4f7db0cd89bc0f9923a6c7c117a392ed902294
facuzavaleta <facuzavaleta890@gmail.com>
2023-09-21 15:49:00 -0300

    Ejercicio 3-3

---
+++ Ejercicios Básicos/3 - Condicionales/Ejercicio3.py
@@ -0,0 +1,7 @@
+dividendo = int(input("Ingrese el dividendo: "))
+divisor = int(input("Ingrese el divisor: "))
+
+if divisor == 0:
+    print("No se puede dividir entre 0.")
+else:
+    print(f"El resultado de la division es {dividendo / divisor}.")
