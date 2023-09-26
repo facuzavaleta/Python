685c5abc7eb3c4b58d93eb8a38f007825b797b74
facuzavaleta <facuzavaleta890@gmail.com>
2023-09-26 16:58:07 -0300

    Ejercicio 4-10

---
+++ Ejercicios Básicos/4 - Bucles/Ejercicio10.py
@@ -0,0 +1,8 @@
+n = int(input("Introduce un número entero positivo mayor que 2: "))
+i = 2
+while n % i != 0:
+    i += 1
+if i == n:
+    print(str(n) + " es primo")
+else:
+    print(str(n) + " no es primo")
